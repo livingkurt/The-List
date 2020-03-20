89333adf3dfe407bd02a4fb8e30a73e784538e76
Kurt LaVacque <lavacquek@icloud.com>
2020-03-20 09:50:34 -0500

    Created a background component and placed it in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './components/Background/Background';
+import Background from './components/Background/Background';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Background>
+        {/* <Container>
+          <Router>
+            <div>
+              <Header />
+              <div className="content_div">
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/blog" component={Blog} />
+                <Route exact path="/blog/led-matrix" component={LED_Matrix} />
+                <Route exact path="/projects" component={Projects} />
+                <Route path="/contact" component={Contact} />
+              </div>
+            </div>
+          </Router>
+        </Container> */}
+      </Background>
     </div>
   );
 }
---
+++ client/src/components/Background/Background.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Container from "../Container/Container"
+import './background.css'
+
+function Background({ children }) {
+  return (
+    <div id="bg">
+      {children}
+    </div>
+  );
+}
+
+export default Background;
